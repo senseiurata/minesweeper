097bf10c5134da8c70936929f384c230fa482290
Ken Urata <senseiurata@gmail.com>
2014-04-07 14:50:35 -0700

    add board and printing capability

--- minesweeper.rb
+++ minesweeper.rb
@@ -5,37 +5,68 @@ end
 class Board
   ROWS = 9
   COLS = 9
+  BOMBS = 10
 
   def initialize
-
     create
   end
 
   def create
     @board = Array.new(9) {Array.new}
-    BOARD_ROWS.times do |row|
-      BOARD_COLS.times do |col|
+    ROWS.times do |row|
+      COLS.times do |col|
         @board[row] << Tile.new([row, col])
       end
     end
+
+    ROWS.times do |row|
+      COLS.times do |col|
+        @board.set_neighbor(@board[row][col], [row, col])
+        @board[row][col].set_neighbors
+      end
+    end
+
+
     @board
   end
 
   def show
+    print "  "
+    COLS.times do |col|
+      print "#{col} "
+    end
+    puts
 
+    ROWS.times do |row|
+      print "#{row} "
+      COLS.times do |col|
+        print "#{@board[row][col].status} "
+      end
+      puts
+    end
   end
 
-  def place_bombs(num_mines)
+  def place_bombs
+    placed_bombs = 0
 
+    until placed_bombs == BOMBS
+      temp_row, temp_col = rand(ROWS), rand(COLS)
+      unless @board[temp_row][temp_col].bombed?
+        @board[temp_row][temp_col].bombed = true
+        placed_bombs += 1
+      end
+    end
   end
 
-  def update_board(pos)
+  def update(pos)
 
   end
 
 end
 
 class Tile
+  attr_writer :bombed
+
   def initialize(pos)
     @bombed = false
     @flagged = false
@@ -43,15 +74,42 @@ class Tile
     @pos = pos
   end
 
-  def reveal
+  def bombed?
+    @bombed
+  end
 
+  def status
+    if @bombed
+      return "*"
+    else
+      return "-"
+    end
+    # else
+    #
+    # if bombed
+    #
+    # if flagged
+  end
+
+  def reveal
+    @flagged = false
   end
 
   def neighbors
-
+    adj_diffs = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+    adj_pos = adj_diffs.map { |row, col| [@pos.first + row, @pos.last + col] }
+    adj_pos.select! { |row, col| row >= 0 && row < ROWS}
   end
 
   def neighbor_bomb_count
 
   end
 end
+
+b = Board.new
+b.place_bombs
+
+b.show
+
+t = Tile.new([4, 4])
+t.neighbors
